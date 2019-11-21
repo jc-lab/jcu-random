fd47dbb8cbdb271df6422280d8ef3d6ec1d2d1b4
Jichan <development@jc-lab.net>
2019-11-21 13:49:37 +0900

    Improve getTempSeed more randomly

--- src/random-intl.cc
+++ src/random-intl.cc
@@ -9,25 +9,47 @@
 
 #include <config-intl.h>
 
+#include <atomic>
+
 #ifdef HAVE_TIME_H
 #include <time.h>
 #endif
 #ifdef HAVE_GETTICKCOUNT
 #include <windows.h>
 #endif
+#ifdef HAVE_GETTIMEOFDAY
+#include <sys/time.h>
+#endif
 
 namespace jcu {
     namespace random {
 
+        static std::atomic<uint64_t> old_seed_(1);
+
+        static void addSeed(uint64_t *target, uint64_t value) {
+            uint64_t out = *target * 0x5DEECE66DULL;
+            out += value;
+            *target = out;
+        }
+
         uint64_t getTempSeed() {
-            uint64_t seed = 0;
+            uint64_t seed = old_seed_.load();
 #ifdef HAVE_TIME_H
-            seed += time(NULL);
+            addSeed(&seed, time(NULL));
 #endif
 #ifdef HAVE_GETTICKCOUNT
-            seed <<= 32u;
-            seed += ::GetTickCount();
+            addSeed(&seed, ::GetTickCount());
 #endif
+#ifdef HAVE_GETTIMEOFDAY
+            {
+                timeval tv = {0, 0};
+                ::gettimeofday(&tv, NULL);
+                addSeed(&seed, tv.tv_sec);
+                addSeed(&seed, tv.tv_usec);
+            }
+#endif
+
+            old_seed_.store(seed);
             return seed;
         }
 
